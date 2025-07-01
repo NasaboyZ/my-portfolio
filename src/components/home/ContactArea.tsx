ba8819b0e2267dbda7cc5bc391d74c5091138a64
NazaboyZ <josefleite.00@hotmail.com>
2025-07-01 15:21:18 +0200

    Email-component beatbeitet

--- src/components/home/ContactArea.tsx
+++ src/components/home/ContactArea.tsx
@@ -1,20 +1,17 @@
-
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 
 export default function ContactArea() {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     // Add form submission logic here
-    console.log('Form submitted:', { name, email, subject, message });
+    console.log("Form submitted:", { name, email, subject, message });
   };
-
 
   return (
     <>
@@ -30,67 +27,78 @@ export default function ContactArea() {
           <div className="row">
             <div className="col-lg-4">
               <div className="contact-content-part  wow fadeInUp delay-0-2s">
-
-                <div className="single-contact wow fadeInUp" data-wow-delay=".2s">
+                <div
+                  className="single-contact wow fadeInUp"
+                  data-wow-delay=".2s"
+                >
                   <span className="circle-btn">
                     <i className="ri-map-pin-line"></i>
                   </span>
-                  <h2>our office:</h2>
-                  <p>Jurain,Dhaka Bangladesh</p>
+                  <h2>Standort:</h2>
+                  <p>Basel, Schweiz</p>
                 </div>
 
-
-                <div className="single-contact wow fadeInUp" data-wow-delay=".4s">
-                  <span className="circle-btn">
-                    <i className="ri-headphone-line"></i>
-                  </span>
-                  <h2>contact number:</h2>
-                  <p>+1234321321</p>
-                </div>
-
-
-                <div className="single-contact wow fadeInUp" data-wow-delay=".6s">
+                <div
+                  className="single-contact wow fadeInUp"
+                  data-wow-delay=".6s"
+                >
                   <span className="circle-btn">
                     <i className="ri-mail-line"></i>
                   </span>
-                  <h2>Email us:</h2>
-                  <p>websitename@mail.com</p>
+                  <h2>Email:</h2>
+                  <p>josefleite.00@hotmail.com</p>
                 </div>
 
-
-                <div className="single-contact wow fadeInUp" data-wow-delay=".6s">
+                <div
+                  className="single-contact wow fadeInUp"
+                  data-wow-delay=".6s"
+                >
                   <h2>Socials</h2>
                   <div className="about-social">
                     <ul>
-                      <li><a target='_blank' href="https://facebook.com"><i className="ri-facebook-circle-fill"></i></a></li>
-                      <li><a target='_blank' href="https://twitter.com"><i className="ri-twitter-x-line"></i></a></li>
-                      <li><a target='_blank' href="https://linkedin.com"><i className="ri-linkedin-fill"></i></a></li>
-                      <li><a target='_blank' href="https://github.com/jamilrayhan10"><i className="ri-github-line"></i></a></li>
+                      <li>
+                        <a
+                          target="_blank"
+                          href="https://www.linkedin.com/in/josef-leite"
+                        >
+                          <i className="ri-linkedin-fill"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a target="_blank" href="https://github.com/NasaboyZ">
+                          <i className="ri-github-line"></i>
+                        </a>
+                      </li>
                     </ul>
                   </div>
                 </div>
-
               </div>
-            </div> 
+            </div>
 
             <div className="col-lg-8">
               <div className="contact-form contact-form-area wow fadeInUp delay-0-4s">
-                <form id="contactForm" className="contact-form" onSubmit={handleSubmit}>
+                <form
+                  id="contactForm"
+                  className="contact-form"
+                  onSubmit={handleSubmit}
+                >
                   <div className="row">
                     <div className="col-md-6">
                       <div className="form-group">
-                        <label htmlFor="name">Full Name</label>
+                        <label htmlFor="name">Kompletten Namen</label>
                         <input
                           type="text"
                           id="name"
                           className="form-control"
                           value={name}
                           onChange={(e) => setName(e.target.value)}
-                          placeholder="Steve Milner"
+                          placeholder="Seven Meier"
                           required
                           data-error="Please enter your Name"
                         />
-                        <label htmlFor="name" className="for-icon"><i className="far fa-user"></i></label>
+                        <label htmlFor="name" className="for-icon">
+                          <i className="far fa-user"></i>
+                        </label>
                         <div className="help-block with-errors"></div>
                       </div>
                     </div>
@@ -103,17 +111,19 @@ export default function ContactArea() {
                           className="form-control"
                           value={email}
                           onChange={(e) => setEmail(e.target.value)}
-                          placeholder="hello@websitename.com"
+                          placeholder="hello@hotmail.com"
                           required
                           data-error="Please enter your Email"
                         />
-                        <label htmlFor="email" className="for-icon"><i className="far fa-envelope"></i></label>
+                        <label htmlFor="email" className="for-icon">
+                          <i className="far fa-envelope"></i>
+                        </label>
                         <div className="help-block with-errors"></div>
                       </div>
                     </div>
                     <div className="col-md-12">
                       <div className="form-group">
-                        <label htmlFor="subject">Subject</label>
+                        <label htmlFor="subject">Betreff</label>
                         <input
                           type="text"
                           id="subject"
@@ -124,13 +134,15 @@ export default function ContactArea() {
                           required
                           data-error="Please enter your Subject"
                         />
-                        <label htmlFor="subject" className="for-icon"><i className="far fa-user"></i></label>
+                        <label htmlFor="subject" className="for-icon">
+                          <i className="far fa-user"></i>
+                        </label>
                         <div className="help-block with-errors"></div>
                       </div>
                     </div>
                     <div className="col-md-12">
                       <div className="form-group">
-                        <label htmlFor="message">Your Message</label>
+                        <label htmlFor="message">Nachricht</label>
                         <textarea
                           name="message"
                           id="message"
@@ -138,7 +150,7 @@ export default function ContactArea() {
                           rows={4}
                           value={message}
                           onChange={(e) => setMessage(e.target.value)}
-                          placeholder="Write Your message"
+                          placeholder="Schreibe deine Nachricht"
                           required
                           data-error="Please Write your Message"
                         ></textarea>
@@ -148,24 +160,28 @@ export default function ContactArea() {
                     <div className="col-md-12">
                       <div className="form-group mb-0">
                         <button type="submit" className="theme-btn">
-                          Send Me Message <i className="ri-mail-line"></i>
+                          sende mir deine Nachricht{" "}
+                          <i className="ri-mail-line"></i>
                         </button>
                         <div id="msgSubmit" className="hidden"></div>
                       </div>
                     </div>
                     <div className="col-md-12 text-center">
-                      <p className="input-success">We have received your mail, We will get back to you soon!</p>
-                      <p className="input-error">Sorry, Message could not send! Please try again.</p>
+                      <p className="input-success">
+                        We have received your mail, We will get back to you
+                        soon!
+                      </p>
+                      <p className="input-error">
+                        Sorry, Message could not send! Please try again.
+                      </p>
                     </div>
                   </div>
                 </form>
               </div>
             </div>
-
           </div>
         </div>
       </section>
-
     </>
-  )
+  );
 }
